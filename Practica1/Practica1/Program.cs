e1602f8a13fb9cc5bc98e0a066edb8c123583b17
RedBrickBurrito <a01568010@gmail.com>
2019-04-22 12:53:00 -0600

    b1

--- Practica1/Practica1/Program.cs
+++ Practica1/Practica1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Practica1
 {
@@ -10,6 +11,7 @@ namespace Practica1
     {
         static void Main(string[] args)
         {
+            List<string> BancodePalabras = File.ReadLines(@"D:\TareaGit1\Numeros.txt").ToList();
         }
     }
 }
