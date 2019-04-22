19ced119bc56fe359e2aca8e45ddf22c660ab2cb
RedBrickBurrito <a01568010@gmail.com>
2019-04-22 14:04:45 -0600

    version-1.0

--- Practica1/Practica1/Program.cs
+++ Practica1/Practica1/Program.cs
@@ -11,7 +11,21 @@ namespace Practica1
     {
         static void Main(string[] args)
         {
-            List<string> BancodePalabras = File.ReadLines(@"D:\TareaGit1\Numeros.txt").ToList();
+            string Numeros = File.ReadAllText(@"D:\TareaGit1\Numeros.txt").ToString();
+            string[] integerStrings = Numeros.Split(new char[] { ' ', '\t', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            int[] integers = new int[integerStrings.Length];
+            for (int n = 0; n < integerStrings.Length; n++)
+            {
+                integers[n] = int.Parse(integerStrings[n]);
+
+                if(integers[n]%10 == 0)
+                {
+                    Console.WriteLine(integers[n] + " Es factor de 10");
+                } 
+            }
+                
+
+            
         }
     }
 }
